8133653cc6e9c19cb64df83e9a174930f373326a
zps仙辉 <1979969842@qq.com>
2020-07-12 22:32:49 +0800

    新建项目

---
+++ src/main/java/com/zps/imchat/ImchatApplication.java
@@ -0,0 +1,13 @@
+package com.zps.imchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImchatApplication.class, args);
+    }
+
+}
