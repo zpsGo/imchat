28e185eb2acfa44e8f4758009412bc6eb157be8c
zps仙辉 <1979969842@qq.com>
2020-07-13 00:40:26 +0800

    新建项目

---
+++ src/test/java/com/zps/imchat/ImchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zps.imchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
