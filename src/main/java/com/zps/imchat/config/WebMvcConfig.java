c42aa72d26d5613d70a9d65c6aacaa3ab32bdaf4
zps仙辉 <1979969842@qq.com>
2020-07-13 08:58:36 +0800

    修改

---
+++ src/main/java/com/zps/imchat/config/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.zps.imchat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: zps
+ * @desc:主要是解决前后端请求的跨域问题
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOrigins("http://localhost:8081")
+                //是否发送cookie信息
+                .allowCredentials(true)
+                //放行哪些请求方法
+                .allowedMethods("*")
+                //放行哪些头部信息
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+}
---
+++ src/main/java/com/zps/imchat/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.zps.imchat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zps
+ **/
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "hello world";
+    }
+}
