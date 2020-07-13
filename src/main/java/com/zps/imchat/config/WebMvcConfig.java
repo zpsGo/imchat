57df9f8189b2f24102022f2df1d6ee46cef55f31
zps仙辉 <1979969842@qq.com>
2020-07-13 10:55:02 +0800

    修改

--- src/main/java/com/zps/imchat/config/WebMvcConfig.java
+++ src/main/java/com/zps/imchat/config/WebMvcConfig.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //设置允许跨域的路径
